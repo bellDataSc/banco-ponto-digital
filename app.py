d389b08b0c088054db85016b24fba6263f626d38
Isabel Cruz <93488292+bellDataSc@users.noreply.github.com>
2025-08-01 18:58:57 +0000

    Corrige bug no formulário de login

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ menu = st.sidebar.radio('Menu', ['Marcar Ponto', 'Relatório Diário', 'Exportar
 
 # Marcação de ponto
 if menu == 'Marcar Ponto':
-    st.subheader('Registrar Ponto')
+    st.subheader('bate... bate... bate!')
     nome = st.text_input('Nome do colaborador')
     cargo = st.text_input('Cargo')
     data = st.date_input('Data', value=date.today())
